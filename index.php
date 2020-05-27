3a4af79f526fb8233d556e32c927d4da5af36d83
Elendev <jonas.renaudot@gmail.com>
2020-05-27 22:52:00 +0200

    WIP

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ $repo = 'https://packages.drupal.org/8'; // 'https://packagist.org';
 
 $repo = new ComposerRepository(['url' => $repo], new NullIO(), Factory::createConfig());
 
+$pool = new \Composer\DependencyResolver\Pool('dev');
+
 $providerNames = $repo->getProviderNames();
 
 $current = 0;
@@ -20,22 +22,23 @@ $packages = [];
 
 foreach ($providerNames as $providerName) {
     echo $providerName . "\n";
-    $currentPackage = $repo->findPackage($providerName, '*');
+    $currentPackages = $repo->whatProvides($pool, $providerName);
 
-    if (!empty($currentPackage)) {
-        $packages[] = $currentPackage;
+    if (!empty($currentPackages)) {
+        $packages = array_merge($packages, $currentPackages);
         $current ++;
     } else {
         echo "Package $providerName not found\n";
     }
 
-    if ($current > 100) {
+    if ($current > 1000) {
         break;
     }
 }
 
 $packageBuilder = new \Composer\Satis\Builder\PackagesBuilder($output, 'repos', [
     'name' => 'My repo',
+    'providers' => true, // Generate a file per project
 ], false);
 
 $packageBuilder->dump($packages);
