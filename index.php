6d8354770417ddcde00f58e872bf5fede18a0f91
Elendev <jonas.renaudot@gmail.com>
2020-05-27 17:36:34 +0200

    WIP

--- index.php
+++ index.php
@@ -7,19 +7,37 @@ use Composer\Config;
 
 require_once "./vendor/autoload.php";
 
+$output = Factory::createOutput();
+
 $repo = 'https://packages.drupal.org/8'; // 'https://packagist.org';
 
 $repo = new ComposerRepository(['url' => $repo], new NullIO(), Factory::createConfig());
 
 $providerNames = $repo->getProviderNames();
 
+$current = 0;
+$packages = [];
+
 foreach ($providerNames as $providerName) {
     echo $providerName . "\n";
-    $package = $repo->findPackage($providerName, '*');
+    $currentPackage = $repo->findPackage($providerName, '*');
 
+    if (!empty($currentPackage)) {
+        $packages[] = $currentPackage;
+        $current ++;
+    } else {
+        echo "Package $providerName not found\n";
+    }
+
+    if ($current > 100) {
+        break;
+    }
 }
 
-$packageBuilder = new \Composer\Satis\Builder\PackagesBuilder();
+$packageBuilder = new \Composer\Satis\Builder\PackagesBuilder($output, 'repos', [
+    'name' => 'My repo',
+], false);
 
+$packageBuilder->dump($packages);
 
 echo 'There are ' . count($providerNames) . ' providers';
