84236454030814b4967070fec812fc2f7591b05f
Jonas Renaudot <jonas.renaudot@swissquote.ch>
2020-05-28 11:59:30 +0200

    WIP

--- index.php
+++ index.php
@@ -1,46 +1,54 @@
 <?php
 
 use Composer\Factory;
+use Composer\IO\ConsoleIO;
 use Composer\Repository\ComposerRepository;
-use Composer\IO\NullIO;
-use Composer\Config;
+use Composer\Satis\Builder\PackagesBuilder;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Filesystem\Filesystem;
 
 require_once "./vendor/autoload.php";
 
+$outputDirectory = __DIR__ . '/repository';
+
 $output = Factory::createOutput();
+
+$io = new ConsoleIO(new ArgvInput(), $output, new HelperSet([]));
 
 $repo = 'https://packages.drupal.org/8'; // 'https://packagist.org';
 
-$repo = new ComposerRepository(['url' => $repo], new NullIO(), Factory::createConfig());
+$repo = new ComposerRepository(['url' => $repo], $io, Factory::createConfig());
 
 $pool = new \Composer\DependencyResolver\Pool('dev');
 
 $providerNames = $repo->getProviderNames();
 
-$current = 0;
+
+$packageBuilder = new PackagesBuilder($output, $outputDirectory, [
+  'name' => 'My repo',
+  'providers' => true, // Generate a file per project
+], false);
+
 $packages = [];
 
 foreach ($providerNames as $providerName) {
-    echo $providerName . "\n";
+    $output->write($providerName . "\n");
     $currentPackages = $repo->whatProvides($pool, $providerName);
 
     if (!empty($currentPackages)) {
         $packages = array_merge($packages, $currentPackages);
-        $current ++;
     } else {
-        echo "Package $providerName not found\n";
-    }
-
-    if ($current > 1000) {
-        break;
+        $output->write("Package $providerName not found\n");
     }
 }
 
-$packageBuilder = new \Composer\Satis\Builder\PackagesBuilder($output, 'repos', [
-    'name' => 'My repo',
-    'providers' => true, // Generate a file per project
-], false);
-
 $packageBuilder->dump($packages);
 
-echo 'There are ' . count($providerNames) . ' providers';
+$fs = new Filesystem();
+
+// Because of the way JFrog works, we have to copy every 'short' JSON into the '/p' directory
+$output->write("Copy package files");
+$fs->mirror($outputDirectory . '/p2', $outputDirectory . '/p');
+
+
